ca25bbe205b3f90a939ad91e98e5782f8ca56aa7
Kevin Zhang <kevinzhang@dhcp-169-233-173-25.cruznet.ucsc.edu>
2018-01-20 02:02:52 -0800

    rip nithin

---
+++ test.js
@@ -0,0 +1,10 @@
+var cv = require('opencv');
+cv.readImage("./starr.jpg", function(err, im){
+  im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
+    for (var i=0;i<faces.length; i++){
+      var x = faces[i]
+      im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+    }
+    im.save('./out.jpg');
+  });
+})
