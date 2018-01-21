4932bf9a10cfab7961a8811456cfdcc6c1eb2318
David Ben-Gurion <rnithin@berkeley.edu>
2018-01-20 22:31:13 -0800

    asdf

--- facemapper/test.js
+++ facemapper/test.js
@@ -1,17 +1,17 @@
 const cv = require('opencv');
 const UPPER_CASCADE="haarcascadetorso.xml"
 
-cv.readImage('./kevin1.jpg', function(err, img) {
+cv.readImage('../otsukevin2.png', function(err, img) {
   if(err) throw err;
   if(img.width() < 1 || img.height() < 1) {
     throw new Error('Image has no size');
   }
 
-  img.detectObject(UPPER_CASCADE,{}, function(err, faces){
+  img.detectObject(UPPER_CASCADE, {}, function(err, faces) {
     if (err) throw err;
-    for (var i = 0; i < faces.length; i++){
-      var face = faces[i];
-      img.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);}
+    //for (var i = 0; i < faces.length; i++)
+      var face = faces[0];
+      img.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
     img.save('changed.png');
   });
 });
