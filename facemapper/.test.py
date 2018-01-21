2380a23ba10bf4588e524704c38f05ef40f3ef8d
David Ben-Gurion <rnithin@berkeley.edu>
2018-01-21 11:26:26 -0800

    hopefully penultimate

--- facemapper/.test.py
+++ facemapper/.test.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import imutils
 from scipy.interpolate import splprep, splev
+import scipy
 import os
 
 def run():
@@ -10,9 +11,26 @@ def run():
 	# Frame1 will be binarized, 2 will be reference and 3
 	# will be used for facial detection purposes
 	frame = cv2.imread('kevin1.jpg')
-	frame2, frame3 = np.copy(frame), np.copy(frame)
 	y, x, c = frame.shape #4032x3024
 	print(y, x, c)
+
+	simg = cv2.imread('../tshirt2.png')
+	ys, xs, cs = simg.shape
+
+	diff_y, diff_x = min(y, ys) / max(y, ys), min(x, xs) / max(x, xs)
+
+	print(diff_y, diff_x)
+	if y > ys:
+		frame = cv2.resize(frame, None, fx = 1, fy = diff_y, interpolation = cv2.INTER_AREA)
+	elif ys < y:
+		simg = cv2.resize(simg, None, fx = 1, fy = diff_y, interpolation = cv2.INTER_AREA)
+	if x > xs:
+		frame = cv2.resize(frame, None, fx = diff_x, fy = 1, interpolation = cv2.INTER_AREA)
+	elif xs < x:
+		simg = cv2.resize(simg, None, fx = diff_x, fy = 1, interpolation = cv2.INTER_AREA)
+
+	frame2, frame3 = np.copy(frame), np.copy(frame)
+	simg2 = np.copy(simg)
 	
 	# Facial Haar classifier; detects faces
 	face_cascade = cv2.CascadeClassifier('haarcascadedefault.xml')
@@ -43,6 +61,7 @@ def run():
 
 	# Drawing the contours
 	M = cv2.moments(contour_max)
+	area_kevin = M['m00']
 	cv2.drawContours(frame, [contour_max], -1, (128,255,0), 40)
 
 	# Finding the centroid of the area using the moment of inertia
@@ -58,17 +77,19 @@ def run():
 	cv2.circle(frame, (cx, cy), 10, (255, 0, 0), 3)
 
 	# Choosing the shirt
-	simg = cv2.imread('../qipao.png')
-	simg = cv2.cvtColor(simg, cv2.COLOR_BGR2GRAY)                    
-	simg = cv2.threshold(simg, 0, 255, cv2.THRESH_OTSU)[1] 
-	ret, frame = cv2.threshold(simg, 127, 255, 0)
+	simg = cv2.cvtColor(simg, cv2.COLOR_BGR2GRAY)
+	ret, simg = cv2.threshold(simg, 127, 255, 0)
 	img, scontours, hierarchys = cv2.findContours(simg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	cv2.imwrite("frame.png", simg)
+	cv2.imwrite("shirtshirt.png", simg)
 	simg_max = max(scontours, key=cv2.contourArea)
 	removearray(scontours, simg_max)
 	simg_contour = max(scontours, key=cv2.contourArea)
 	M = cv2.moments(simg_contour)
-	cv2.drawContours(simg, [simg_contour], -1, (128,255,0), 5)
+	area_shirt = M['m00']
+	scx = int(M["m10"] / M["m00"])
+	scy = int(M["m01"] / M["m00"])
+	cv2.drawContours(simg, [simg_contour], -1, (128,255,0), 20)
+	cv2.circle(simg, (scx, scy), 10, (255, 255, 255), 21)
 	cv2.imwrite('shirtfinal.png', simg)
 	'''
 	interpolation_difference = max(simg_contour.shape[0], contour_max.shape[0])
@@ -82,14 +103,27 @@ def run():
 		contour_max = gaurav[0].reshape(1,-1,2)
 		simg_contour = simg_contour.reshape(1,-1,2)
 	'''
-	f = 8
-	simg_contour = interpolate(simg_contour, f)[0]
-	contour_max = interpolate(contour_max, f)[0]
-	# EstimateRigidTransform
-	affine_matrix = cv2.estimateRigidTransform(simg_contour,contour_max,True)
-	print(affine_matrix)
-	print(simg_contour, contour_max)
+
+	cv2.imwrite("scaled_frame.png", frame)
+	cv2.imwrite("scaled_simg.png", simg)
+	print(frame.shape, simg.shape)
+	print(area_shirt, area_kevin)
 	
+	
+
+	#f = 8
+	#simg_contour_i = interpolate(simg_contour, f)[0]
+	#contour_max_i = interpolate(contour_max, f)[0]
+	
+	# EstimateRigidTransform
+	#affine_matrix = cv2.estimateRigidTransform(simg_contour_i,contour_max_i,True)
+	#print(affine_matrix)
+	#print(contour_max_i)
+
+	#new_img = matrix_to_coordinates(simg, affine_matrix)
+	#cv2.imwrite('test.png', new_img)
+	
+	#print(np.matmul(simg_contour , np.array( [(164, 215), (128, 8), (480, 72)] ).reshape(1,2,-1)))
 
 	#print(simg.shape, frame2.shape)
 	#frame2[cy:cy+simg.shape[0], cx:cx+simg.shape[1]] = simg
@@ -102,7 +136,20 @@ def run():
 	
                '''
 
-	cv2.imwrite('final.png', frame)
+
+	#cx, cy for kevin
+	#scx, scy for shirt
+	rows, cols= simg.shape
+	from scipy import ndimage
+	M = np.float32([[1, 0, cx - scx], [0, 1, cy + 210]])
+	new_shirt = cv2.warpAffine(simg2, M, (cols, rows))
+
+	cv2.imwrite('crap.png', cv2.addWeighted(new_shirt, 0.65, frame2, 0.35, 0))
+	
+	
+
+
+	cv2.imwrite('final.png', frame2)
 	#key = cv2.waitKey(1) & 0xFF
 	#if key == ord("q"):
 	#	break
@@ -133,6 +180,15 @@ def interpolate(contour, difference):
 	res_array = [[[int(i[0]), int(i[1])]] for i in zip(x_new,y_new)]
 	smoothened.append(np.asarray(res_array, dtype=np.int32))
 	return smoothened
+
+def matrix_to_coordinates(matr, transform):
+	new_img = np.empty(shape=[len(matr), len(matr[0])])
+	for i in range(len(matr)):
+		for j in range(len(matr[0])):
+			ncoord = np.matmul(transform, np.asarray([i, j, 1]).T)
+			if ncoord[0] > 0 and ncoord[1] > 0 and ncoord[0] < len(matr) and ncoord[1] < len(matr[0]):
+				new_img[int(ncoord[0])][int(ncoord[1])] = matr[i][j]
+	return new_img
 	
 def main():
 	run()
